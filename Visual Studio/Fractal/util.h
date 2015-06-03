9ec19496e10a4ef799da3d9e3ef3ec84112a2daf
Daniel Grimshaw <danielgrimshaw42@gmail.com>
2015-06-03 07:04:33 -0700

    Started working on new functionality for util

--- Visual Studio/Fractal/util.h
+++ Visual Studio/Fractal/util.h
@@ -5,9 +5,24 @@ unsigned long get_msec(void);
 
 void *load_image(const char *fname, unsigned long *xsz, unsigned long *ysz);
 
-unsigned int setup_shader(const char *vname, const char *fname);
-void set_uniform1f(unsigned int prog, const char *name, float val);
-void set_uniform2f(unsigned int prog, const char *name, float v1, float v2);
-void set_uniform1i(unsigned int prog, const char *name, int val);
+void setDefaultsOpts();
+void setParameter();
+void loadShaders(const char * vname, const char * fname);
+void loadShader(char * type, char * path);
+bool createShader(const char * vertex_src, const char * fragment_src);
+const char * parseShader(const char * type, const char * src);
+unsigned int createShader(const char * type, const char * src, const char * opts);
+//void set_uniform1f(unsigned int prog, const char *name, float val);
+//void set_uniform2f(unsigned int prog, const char *name, float v1, float v2);
+//void set_uniform1i(unsigned int prog, const char *name, int val);
 
+void reset();
+void resize(unsigned int width, unsigned int height);
+unsigned int * getPixels(unsigned int x, unsigned int y, unsigned int width, unsigned int height);
+
+void sendUniforms();
+void sendUniform(int key);
+
+void loadTextures();
+void loadTexture(const char * path);
 #endif	/* _UTIL_H_ */
