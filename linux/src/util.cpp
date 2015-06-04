791524aae8a6a738d5dd9748a73d16c8facd1c35
Daniel Grimshaw <danielgrimshaw42@gmail.com>
2015-06-03 20:48:35 -0700

    Copied changes to linux

--- linux/src/util.cpp
+++ linux/src/util.cpp
@@ -42,6 +42,18 @@ unsigned long get_msec(void) {
 #endif	/* __unix__ */
 }
 
+void Shader::setDefaultsOpts() {
+	opt.width = 800;
+	opt.height = 400;
+	opt.vertex = NULL;
+	opt.fragment = NULL;
+	opt.vpath = NULL;
+	opt.fpath = NULL;
+}
+
+void loadShaders(const char * vname, const char * fname) {
+	
+}
 unsigned int setup_shader(const char * vname, const char * fname) { // vector shader, fragment shader
 	using namespace std;
 	unsigned int prog, vsdr, fsdr;
